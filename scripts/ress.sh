0faf658a583c933a969ce2747aab4dbc0e70950b
Mrid22 <mridulaga@outlook.com>
2026-08-25 08:46:15 +0300

    setup scripts

---
+++ scripts/ress.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+omarchy plugin add https://github.com/tsouth89/omarchy-resurrect --yes
+~/.config/omarchy/plugins/tsouth89.resurrect/bin/ress restore --from https://github.com/Mrid22/omarchy-setup
